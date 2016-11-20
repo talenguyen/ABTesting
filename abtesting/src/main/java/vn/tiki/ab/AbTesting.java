b6b30d3a889c1a7625f6bfa9699b0edf0cce7e90
Ken Zira <shaymin.android@gmail.com>
2016-11-20 22:28:26 +0700

    re add file

---
+++ abtesting/src/main/java/vn/tiki/ab/AbTesting.java
@@ -0,0 +1,30 @@
+package vn.tiki.ab;
+
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
+public class AbTesting {
+
+  private final FirebaseRemoteConfig firebaseRemoteConfig;
+  private final long cacheExpiration;
+
+  /**
+   *
+   * @param settings
+     */
+  public AbTesting(AbSettings settings) {
+    this.firebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+
+    final FirebaseRemoteConfigSettings remoteConfigSettings =
+        new FirebaseRemoteConfigSettings.Builder().setDeveloperModeEnabled(settings.debug())
+            .build();
+    firebaseRemoteConfig.setConfigSettings(remoteConfigSettings);
+
+    firebaseRemoteConfig.setDefaults(settings.defaults());
+    cacheExpiration = settings.cacheExpiration();
+  }
+
+  public RequestBuilder request() {
+    return new RequestBuilder(firebaseRemoteConfig, cacheExpiration);
+  }
+}
